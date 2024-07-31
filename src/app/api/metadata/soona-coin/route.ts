379e17cd664a43508124cd70cc8cae84baec48cc
Soona <code@chainsona.dev>
2024-07-31 23:50:11 +0200

    feat: add soona metadata

---
+++ src/app/api/metadata/soona-coin/route.ts
@@ -0,0 +1,20 @@
+export async function GET() {
+  return Response.json(
+    {
+      name: "Soona Coin",
+      symbol: "SOONA",
+      description:
+        "Created during a Turbin3 course, Soona Coin is a token that will be used to reward the best students of the course.",
+      image: "https://chainsona.dev/chainsona-madlads.png",
+    },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, Content-Encoding, Accept-Encoding",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
